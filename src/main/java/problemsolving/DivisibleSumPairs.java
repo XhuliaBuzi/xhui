29a1ea447b0de45f0ba3193de55d7ab3db509a74
Xhulia <xhuliabuzzi@gmail.com>
2021-10-11 22:30:22 +0200

    update

--- src/main/java/problemsolving/DivisibleSumPairs.java
+++ src/main/java/problemsolving/DivisibleSumPairs.java
@@ -5,25 +5,23 @@ import java.util.Scanner;
 public class DivisibleSumPairs {
     private static final Scanner scanner = new Scanner(System.in);
     private static final int numberOfArray = scanner.nextInt();
-private static final int m = scanner.nextInt();
-    public static void main(String[] args) {
-        int[] intagerArray = new int[numberOfArray];
-        for (int i = 0; i < numberOfArray; i++) {
-            intagerArray[i] = scanner.nextInt();
-        }
-        printCalculator(intagerArray, m);
+    private static final int m = scanner.nextInt();
 
+    public static void main(String[] args) {
+        int[] ints = new int[numberOfArray];
+        for (int i = 0; i < numberOfArray; i++) {
+            ints[i] = scanner.nextInt();
+        }
+        printCalculator(ints, m);
     }
 
     public static void printCalculator(int[] array, int m) {
-        int realsume = 1;
-        int tt=0;
-        for (int j : array) {
-            for (int t = tt; t < array.length; t++) {
-                realsume += (j + array[t]) % m == 0 ? 1 : 0;
+        int r = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int t =i+1; t < array.length; t++) {
+                r+=((array[i]+array[t])%m==0)?1:0;
             }
         }
-        ++tt;
-        System.out.println(realsume);
+        System.out.println(r);
     }
 }

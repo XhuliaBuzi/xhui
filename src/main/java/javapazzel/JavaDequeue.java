12a9838d4c88003e3eb8bbc868f5ee1f621579a9
Xhulia <xhuliabuzzi@gmail.com>
2021-10-11 21:17:04 +0200

    update

--- src/main/java/javapazzel/JavaDequeue.java
+++ src/main/java/javapazzel/JavaDequeue.java
@@ -4,18 +4,18 @@ import java.util.*;
 
 public class JavaDequeue {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final int numberOfArray = scanner.nextInt();
-    private static final int combination = scanner.nextInt();
+    private static final int NUMBER = scanner.nextInt();
+    private static final int COMBINATION = scanner.nextInt();
 
     public static void main(String[] args) {
         int print = 0;
         List<Integer> arr = new ArrayList<>();
         Set<Integer> for_calculate_thinks = new HashSet<>(arr);
-        for (int i = 0; i < numberOfArray; i++) {
+        for (int i = 0; i < NUMBER; i++) {
             int s = scanner.nextInt();
             arr.add(s);
             for_calculate_thinks.add(s);
-            if (arr.size() == combination) {
+            if (arr.size() == COMBINATION) {
                 if (for_calculate_thinks.size() > print) print = for_calculate_thinks.size();
                 int delete = arr.remove(0);
                 if (!arr.contains(delete)) {

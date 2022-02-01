12ffeeff63d592cf9ee4b099f5bceb7d234e3c33
Xhulia <xhuliabuzzi@gmail.com>
2022-02-01 20:01:51 +0100

    update

---
+++ src/main/java/regex/BuildaStackExchangeScraper.java
@@ -0,0 +1,26 @@
+package regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BuildaStackExchangeScraper {
+
+    public static void main(String[] args) throws Exception {
+
+        StringBuilder builder = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            builder.append(sc.nextLine() + "\n");
+        }
+
+        Pattern p = Pattern.compile(
+                "href=\"\\/questions\\/(\\d+)\\/.+?>(.+?)<\\/a>(?:.+?)\"relativetime\">(.+?)<\\/span>", Pattern.DOTALL);
+        Matcher m = p.matcher(builder.toString());
+
+        while (m.find()) {
+            System.out.println(m.group(1) + ";" + m.group(2) + ";" + m.group(3));
+        }
+    }
+}
